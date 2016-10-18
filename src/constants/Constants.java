783af379e4f25fc700cd19e7591f3a5a34b53142
madchemist10 <2012kemp1234@gmail.com>
2016-10-18 17:23:45 -0500

    added constants file to contain frequently used strings

---
+++ src/constants/Constants.java
@@ -0,0 +1,12 @@
+package constants;
+
+/**
+ */
+public class Constants {
+
+    public static final String API_KEY = "9d3c8941bb5a7d5abef3326b3cd2cab8";
+    public static final String TOP_RATED = "top_rated";
+    public static final String NOW_PLAYING = "now_playing";
+    public static final String UPCOMING = "upcoming";
+    public static final String MOVIE_DB_ADR = "https://api.themoviedb.org/";
+}
